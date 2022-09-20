2d63564f0d2d7d3312e27632d705306021fbfb63
Johnadibe <40009354+Johnadibe@users.noreply.github.com>
2022-09-20 13:23:50 +0100

    Modify script.js file

--- script.js
+++ script.js
@@ -2,80 +2,72 @@ const btn = document.querySelector('#btn');
 const list = document.querySelector('#list');
 const form = document.querySelector('form');
 
-class BookObject{
-    constructor(title,author){
-        this.title=title.value,
-        this.author=author.value
+class BookObject {
+  constructor(title, author) {
+    this.title = title.value;
+    this.author = author.value;
+  }
+}
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    const { code, name } = e;
+    return (
+      e instanceof DOMException
+            && (code === 22
+                || code === 1014
+                || name === 'QuotaExceededError'
+                || name === 'NS_ERROR_DOM_QUOTA_REACHED')
+            && storage.length !== 0
+    );
+  }
+}
+
+function getBooks() {
+  let book;
+  if (storageAvailable('localStorage')) {
+    if (localStorage.getItem('booksData') == null) {
+      book = [];
+    } else {
+      book = JSON.parse(localStorage.getItem('booksData'));
     }
+  }
+  return book;
 }
 
-class ClassLocalStorage{
-    static storageAvailable(type) {
-        let storage;
-        try {
-            storage = window[type];
-            const x = '__storage_test__';
-            storage.setItem(x, x);
-            storage.removeItem(x);
-            return true;
-        } catch (e) {
-            const { code, name } = e;
-            return (
-                e instanceof DOMException
-                && (code === 22
-                    || code === 1014
-                    || name === 'QuotaExceededError'
-                    || name === 'NS_ERROR_DOM_QUOTA_REACHED')
-                && storage.length !== 0
-            );
-        }
+function addBooks(book) {
+  const booksList = getBooks();
+  booksList.push(book);
+  localStorage.setItem('booksData', JSON.stringify(booksList));
 }
 
-    static getBooks() {
-        let book;
-        if (this.storageAvailable('localStorage')) {
+const createBookElement = () => {
+  const bookData = getBooks();
 
-            if (localStorage.getItem('booksData') == null) {
-                book = [];
-            }
-            else {
-                book = JSON.parse(localStorage.getItem('booksData'));
-            }
+  bookData.forEach((book) => {
+    const bookContainer = document.createElement('div');
+    bookContainer.className = 'books';
+    bookContainer.innerHTML = `<h2 id="title-name">${book.title}</h2><p id="author-name">${book.author}</p> <button class="remove-btn">Remove</button>`;
+    list.appendChild(bookContainer);
+  });
+};
 
-        }
-        return book;
-}
-
-    static addBooks(book) {
-        const booksList = this.getBooks();
-        booksList.push(book);
-        localStorage.setItem('booksData', JSON.stringify(booksList));
-    }
-}
-
-
-
-
-const createBookElement = (book) => {
-    const bookData = ClassLocalStorage.getBooks();
-
-    bookData.forEach((book) => {
-        const bookContainer = document.createElement('div');
-        bookContainer.className = 'books';
-        bookContainer.innerHTML = `<h2 id="title-name">${book.title}</h2><p id="author-name">${book.author}</p> <button class="remove-btn">Remove</button>`;
-        list.appendChild(bookContainer);
-    });
-}
-    
 createBookElement();
 btn.addEventListener('click', (event) => {
   event.preventDefault();
   const title = document.querySelector('#title');
   const author = document.querySelector('#author');
 
-  const bookCard = new BookObject(title,author);
+  const bookCard = new BookObject(title, author);
 
-  ClassLocalStorage.addBooks(bookCard);
+  addBooks(bookCard);
 
   const bookContainer = document.createElement('div');
   bookContainer.className = 'books';
@@ -90,8 +82,7 @@ const removeBtn = document.querySelector('#list');
 removeBtn.addEventListener('click', (event) => {
   event.target.parentElement.remove();
   const title = event.target.parentElement.firstElementChild.textContent;
-  const books = ClassLocalStorage.getBooks();
+  const books = getBooks();
   const filtered = books.filter((book) => book.title !== title);
   localStorage.setItem('booksData', JSON.stringify(filtered));
 });
-
