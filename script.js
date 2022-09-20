9453fc70e69b4fb3787d1ea301c7b3648015018b
natanimabesha <natanimabesha@www.zhewutech.com>
2022-09-20 13:22:28 +0300

    Fix refactoring with class

--- script.js
+++ script.js
@@ -3,62 +3,62 @@ const list = document.querySelector("#list");
 const form = document.querySelector("form");
 const storage = window.localStorage;
 
-function bookObject(title,author){
-    const books = {
-        title: title.value,
-        author: author.value,
-    };
-
-    return books;
-}
-
-
-function storageAvailable(type) {
-    let storage;
-    try {
-        storage = window[type];
-        const x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-    } catch (e) {
-        const { code, name } = e;
-        return (
-            e instanceof DOMException
-            && (code === 22
-                || code === 1014
-                || name === 'QuotaExceededError'
-                || name === 'NS_ERROR_DOM_QUOTA_REACHED')
-            && storage.length !== 0
-        );
+class BookObject{
+    constructor(title,author){
+        this.title=title.value,
+        this.author=author.value
     }
 }
 
-function getBooks(){
-    let book;
-    if (storageAvailable('localStorage')) {
-
-        if(localStorage.getItem('booksData') == null){
-            book = [];
+class ClassLocalStorage{
+    static storageAvailable(type) {
+        let storage;
+        try {
+            storage = window[type];
+            const x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch (e) {
+            const { code, name } = e;
+            return (
+                e instanceof DOMException
+                && (code === 22
+                    || code === 1014
+                    || name === 'QuotaExceededError'
+                    || name === 'NS_ERROR_DOM_QUOTA_REACHED')
+                && storage.length !== 0
+            );
         }
-        else{
-            book = JSON.parse(localStorage.getItem('booksData'));
-        }
-        
-    }  
-    return book;
 }
 
-function addBooks(book){
-    const booksList =  getBooks();
-    booksList.push(book);
-    localStorage.setItem('booksData', JSON.stringify(booksList));
+    static getBooks() {
+        let book;
+        if (this.storageAvailable('localStorage')) {
+
+            if (localStorage.getItem('booksData') == null) {
+                book = [];
+            }
+            else {
+                book = JSON.parse(localStorage.getItem('booksData'));
+            }
+
+        }
+        return book;
 }
+
+    static addBooks(book) {
+        const booksList = this.getBooks();
+        booksList.push(book);
+        localStorage.setItem('booksData', JSON.stringify(booksList));
+    }
+}
+
 
 
 
 const createBookElement = (book) => {
-    const bookData = getBooks();
+    const bookData = ClassLocalStorage.getBooks();
 
     bookData.forEach((book) => {
         const bookContainer = document.createElement('div');
@@ -76,9 +76,9 @@ btn.addEventListener("click", (event) => {
     var title = document.querySelector("#title");
     var author = document.querySelector("#author");
 
-    const bookCard = bookObject(title,author);
+    const bookCard = new BookObject(title,author);
 
-    addBooks(bookCard);
+    ClassLocalStorage.addBooks(bookCard);
 
     const bookContainer = document.createElement('div');
     bookContainer.className = 'books';
@@ -97,7 +97,7 @@ const removeBtn = document.querySelector("#list");
 removeBtn.addEventListener('click',(event) => {
     event.target.parentElement.remove();
     var title = event.target.parentElement.firstElementChild.textContent;
-    const books = getBooks();
+    const books = ClassLocalStorage.getBooks();
     let filtered;
     filtered = books.filter((book) => book.title !== title);
     localStorage.setItem('booksData', JSON.stringify(filtered));
